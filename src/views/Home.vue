766b00090f40ecc795020b5278bdfef0e57d9fea
Phoebe <jolinhappy1990@hotmail.com>
2021-01-17 22:21:05 +0800

    project init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="navbar">
+      <div class="navbar-icon">
+        <img src="" class="icon" alt="" />
+      </div>
+      <div class="navbar-item-group">
+        <a href="" class="navbar-item">關於文科</a>
+        <a href="" class="navbar-item">文科會的技能</a>
+        <a href="" class="navbar-item">文科的作品</a>
+        <a href="" class="navbar-item">文科的經歷</a>
+        <a href="" class="navbar-item">了解更多文科</a>
+      </div>
+      <div class="navbar-contect-group">
+        <a href="" class="contect">git</a>
+        <a href="" class="contect">med</a>
+        <a href="" class="contect">mail</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
@@ -16,3 +31,11 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+  // background: red;
+}
+</style>
